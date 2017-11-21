5b946590ebe16f3f6358db01eab6bc9c006f3d3f
Adrian Ren <adrian.ren@outlook.com>
2017-11-22 11:16:17 +1300

    reducer and book_list created

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+// step 1: create the reducer
+// step 2: wire reducer into our application 
+export default function () {
+  return [
+    {title: 'Harry Potter'},
+    {title: 'One Day'},
+    {title: 'The Dark Tower'},
+    {title: 'JavaScript is a good language'}
+  ]
+}
